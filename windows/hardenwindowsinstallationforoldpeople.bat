cd15a7ca63daf04b9646e323481f0de0e1eaef4f
Slayer Ranger <tanc7@unlv.nevada.edu>
2022-11-19 17:11:59 -0800

    Added hardening commands for old people

---
+++ windows/hardenwindowsinstallationforoldpeople.bat
@@ -0,0 +1,10 @@
+# Make a admin account with a really long password as Admin, and make new user accounts for old people that get tricked into being pwned all the time. As your admin user, run all of this. 
+netsh advfirewall firewall add rule name="BlockHostile" dir=in action=block protocol=TCP localport=135,139,445,808,5040,7680,9001,139,49664-49669,50336,3389
+#CHECK netstat -ano | findstr /i LISTEN first!
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard" /v "EnableVirtualizationBasedSecurity" /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard" /v "RequirePlatformSecurityFeatures" /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard" /v "Locked" /t REG_DWORD /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity" /v "Enabled" /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity" /v "Locked" /t REG_DWORD /d 0 /f
+setx /M MP_FORCE_USE_SANDBOX 1
+powershell.exe -ep bypass "Set-MpPreference -HighThreatDefaultAction Remove -Force;Set-MpPreference -LowThreatDefaultAction Remove -Force;Set-MpPreference -MAPSReporting Advanced -Force;Set-MpPreference -ModerateThreatDefaultAction Remove -Force;Set-MpPreference -SevereThreatDefaultAction Remove -Force;Set-MpPreference -UnknownThreatDefaultAction Remove -Force;Set-MpPreference -DisableRealtimeMonitoring $false -Force;Set-MpPreference -DisableArchiveScanning $false -Force;Set-MpPreference -DisableBehaviorMonitoring $false -Force;Set-MpPreference -DisableBlockAtFirstSeen $false -Force;Set-MpPreference -DisableEmailScanning $false -Force;Set-MpPreference -DisableIntrusionPreventionSystem $false -Force;Set-MpPreference -DisableIOAVProtection $false -Force;Set-MpPreference -DisableRealTimeMonitoring $false -Force;Set-MpPreference -DisableScriptScanning $false -Force;Set-ExecutionPolicy restricted -Force;Set-MpPreference -PUAProtection Enabled"
